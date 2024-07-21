13dc47bfc1965e501fe93470a974a0765f7ea08d
quantumcheese0-admin <112054532+CheesyKiller@users.noreply.github.com>
2024-07-20 22:52:16 -0700

    first commit

---
+++ CSC500-1/SimpleMath.py
@@ -0,0 +1,14 @@
+# Author: Jacob Abts
+# Submission Date: 7/21/2024
+# CSU Global - CSC500-1
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a second number: "))
+
+# Part 1
+print(a, "+", b, "=", a + b)
+print(a, "-", b, "=", a - b)
+
+# Part 2
+print(a, "*", b, "=", a * b)
+print(a, "/", b, "=", int(a / b))
